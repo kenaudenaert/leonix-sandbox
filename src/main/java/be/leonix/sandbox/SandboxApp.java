9c4ffc529be262b328a69e5301ff0307c15dd202
leonix <leonix@localhost>
2020-01-18 15:18:36 +0100

    Added config files.

--- src/main/java/be/leonix/sandbox/SandboxApp.java
+++ src/main/java/be/leonix/sandbox/SandboxApp.java
@@ -1,13 +1,20 @@
 package be.leonix.sandbox;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SandboxApp {
-	
+
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(SandboxApp.class);
-		application.run(args);
+		
+		// Ensure that we are using the application properties we need.
+		String[] arguments = Arrays.copyOf(args, args.length + 1);
+		arguments[arguments.length - 1] = "--spring.config.name=sandbox";
+		
+		application.run(arguments);
 	}
 }
