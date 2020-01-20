0edf4d787b873a3e35c52d876db4c2e5e6a7fbca
leonix <leonix@localhost>
2020-01-20 02:17:35 +0100

    Added jongo stuff.

--- src/main/java/be/leonix/sandbox/repository/MovieRepositoryImpl.java
+++ src/main/java/be/leonix/sandbox/repository/MovieRepositoryImpl.java
@@ -1,9 +1,15 @@
 package be.leonix.sandbox.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.jongo.MongoCursor;
 
 import be.leonix.sandbox.model.Movie;
 
@@ -12,29 +18,36 @@ import be.leonix.sandbox.model.Movie;
  */
 @Repository
 public class MovieRepositoryImpl implements MovieRepository {
-
+	
+	private final MongoCollection movies;
+	
+	@Autowired
+	public MovieRepositoryImpl(Jongo jongo) {
+		movies = jongo.getCollection("movies");
+	}
+	
 	@Override
 	public List<Movie> findAllMovies() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Movie> allMovies = new ArrayList<>();
+		MongoCursor<Movie> cursor = movies.find().as(Movie.class);
+		cursor.forEach(movie -> allMovies.add(movie));
+		return allMovies;
 	}
-
+	
 	@Override
 	public Optional<Movie> findMovieById(String movieId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	@Override
 	public void addMovie(Movie movie) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void updateMovie(Movie movie) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
