04f3cf9c5d8e97da641566ee80eb439f2a43a0e1
leonix <leonix@localhost>
2020-01-24 14:55:28 +0100

    Added test annotations.

--- src/test/java/be/leonix/sandbox/resource/UserResourceTest.java
+++ src/test/java/be/leonix/sandbox/resource/UserResourceTest.java
@@ -1,5 +1,13 @@
 package be.leonix.sandbox.resource;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
 public class UserResourceTest {
 
+	@Test
+	void contextLoads() {
+		
+	}
 }
--- src/test/java/be/leonix/sandbox/service/UserServiceTest.java
+++ src/test/java/be/leonix/sandbox/service/UserServiceTest.java
@@ -1,8 +1,13 @@
 package be.leonix.sandbox.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig
 public class UserServiceTest {
 
+	@Test
+	void contextLoads() {
+		
+	}
 }
