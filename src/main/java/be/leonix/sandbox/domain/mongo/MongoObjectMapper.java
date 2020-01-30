b1400d2f1accba2175f514e09ea3d33dbc396053
leonix <leonix@localhost>
2020-01-30 13:08:30 +0100

    Added configuration.

--- src/main/java/be/leonix/sandbox/domain/mongo/MongoObjectMapper.java
+++ src/main/java/be/leonix/sandbox/domain/mongo/MongoObjectMapper.java
@@ -1,7 +1,11 @@
 package be.leonix.sandbox.domain.mongo;
 
-import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * The {@link ObjectMapper} for Mongo document conversion.
@@ -13,10 +17,28 @@ public class MongoObjectMapper extends ObjectMapper {
 	private static final long serialVersionUID = 1L;
 	
 	public MongoObjectMapper() {
-		this(null);
-	}
-	
-	public MongoObjectMapper(JsonFactory jsonFactory) {
-		super(jsonFactory);
+		// Support for Java Time API types.
+		registerModule(new JavaTimeModule());
+		
+		// Support for the Optional<T> type.
+		registerModule(new Jdk8Module());
+		
+		configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		
+		// Only support serialization using annotated fields.
+		setVisibility(getSerializationConfig().getDefaultVisibilityChecker()
+				.withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+				.withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+		
+		// Only support deserialization using annotated fields.
+		setVisibility(getSerializationConfig().getDefaultVisibilityChecker()
+				.withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+				.withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
 	}
 }
