55bb873322ed29a1e2caa06a7d66847a5cb5273f
kenaudenaert <ken.audenaert@telenet.be>
2021-08-04 17:35:44 +0200

    Added RefactorContex.

---
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/RefactorContext.java
@@ -0,0 +1,54 @@
+package be.leonix.tools;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+
+/**
+ * This class encapsulates the context for a refactor-operation.
+ * 
+ * @author Ken Audenaert
+ */
+public final class RefactorContext {
+	
+	private final RefactorMode mode;
+	private final List<String> infoLines;
+	
+	private RefactorContext(RefactorMode mode) {
+		this.mode = Objects.requireNonNull(mode);
+		infoLines = new ArrayList<String>();
+	}
+	
+	public RefactorMode getMode() {
+		return mode;
+	}
+	
+	public List<String> getInfo() {
+		return Collections.unmodifiableList(infoLines);
+	}
+	
+	public void addInfo(String infoLine) {
+		infoLines.add(infoLine);
+	}
+	
+	/**
+	 * Logs the collected info for the refactor.
+	 */
+	public void logInfo() {
+		for (String infoLine : infoLines) {
+			System.out.println(infoLine);
+		}
+	}
+	
+	/**
+	 * Logs the collected info for the refactor.
+	 */
+	public void logInfo(Logger logger) {
+		for (String infoLine : infoLines) {
+			logger.info(infoLine);
+		}
+	}
+}
