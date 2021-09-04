e55ab4e58752ce9c3292980f4b40182979afd812
Ken Audenaert <ken.audenaert@telenet.be>
2021-09-04 12:01:31 +0200

    added git support.

---
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/SourceAuthor.java
@@ -0,0 +1,27 @@
+package be.leonix.tools.refactor.model;
+
+import java.util.Objects;
+
+/**
+ * This class defines an (immutable) source-author.
+ * 
+ * @author Ken Audenaert
+ */
+public final class SourceAuthor {
+	
+	private final String name;
+	private final String email;
+	
+	public SourceAuthor(String name, String email) {
+		this.name  = Objects.requireNonNull(name);
+		this.email = Objects.requireNonNull(email);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+}
