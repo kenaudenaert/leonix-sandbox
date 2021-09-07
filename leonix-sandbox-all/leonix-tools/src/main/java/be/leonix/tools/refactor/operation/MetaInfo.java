83124d4bf3b1c3cb03385a7b5209127fdaf397b2
kenaudenaert <ken.audenaert@telenet.be>
2021-09-07 12:02:47 +0200

    initial implementation.

---
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/operation/MetaInfo.java
@@ -0,0 +1,125 @@
+package be.leonix.tools.refactor.operation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author leonix
+ */
+public final class MetaInfo {
+	
+	private static final Pattern META_INFO_DEF = Pattern.compile(
+			"public static final String ([\\w_]+) = \"([\\w_]+)\";");
+	
+	private final String infoClass;
+	private final String packageID;
+	private final String keyPrefix;
+	
+	private final Map<String, String> metaConstants = new LinkedHashMap<>();
+	
+	public MetaInfo(File metaInfoFile) {
+		
+		// Check the class-name of the meta-info.
+		String fileName = metaInfoFile.getName();
+		if (! fileName.endsWith("Meta.java")) {
+			throw new RuntimeException("Invalid (type-name) MetaInfo file: " + fileName);
+		}
+		infoClass = StringUtils.removeEnd(fileName, "Meta.java");
+		
+		// Get the package and constants.
+		try (FileReader fileReader = new FileReader(metaInfoFile, StandardCharsets.UTF_8)) {
+			try (BufferedReader reader = new BufferedReader(fileReader)) {
+				String line = reader.readLine();
+				
+				// Get the package-name of the meta-info.
+				if (line != null && line.startsWith("package ") && line.endsWith(";")) {
+					packageID = line.substring("package ".length(), line.length() - 1);
+				} else {
+					throw new RuntimeException("Invalid (no package) MetaInfo file: " + fileName);
+				}
+				
+				line = reader.readLine();
+				while (line != null) {
+					Matcher matcher = META_INFO_DEF.matcher(line);
+					if (matcher.find()) {
+						String constant = matcher.group(1);
+						String literal  = matcher.group(2);
+						metaConstants.put(literal, constant);
+					}
+					line = reader.readLine();
+				}
+			}
+		} catch (RuntimeException | IOException ex) {
+			throw new RuntimeException("Could not parse meta-info-file: " + metaInfoFile, ex);
+		}
+		
+		// Get the prefix and filter on it.
+		List<String> keys = new ArrayList<>(metaConstants.keySet());
+		Collections.reverse(keys);
+		String lastKey = keys.iterator().next();
+		if (lastKey == null) {
+			throw new RuntimeException("Invalid (no prefi) MetaInfo file: " + fileName);
+		} else {
+			keyPrefix = lastKey.substring(0, lastKey.indexOf('_'));
+			
+			Iterator<String> iterator = metaConstants.keySet().iterator();
+			while (iterator.hasNext()) {
+				String value = iterator.next();
+				if (! value.startsWith(keyPrefix + "_")) {
+					iterator.remove();
+				}
+			}
+		}
+	}
+	
+	public String getInfoClass() {
+		return infoClass;
+	}
+	
+	public String getPackageID() {
+		return packageID;
+	}
+	
+	public String getKeyPrefix() {
+		return keyPrefix;
+	}
+	
+	public Map<String, String> getMetaConstants() {
+		return Collections.unmodifiableMap(metaConstants);
+	}
+	
+	public static Map<String, MetaInfo> getMetaInfo(File metaInfoDir) {
+		Map<String, MetaInfo> metaInfos = new LinkedHashMap<>();
+		
+		File[] metaInfoFiles = metaInfoDir.listFiles();
+		if (metaInfoFiles != null) {
+			for (File metaInfoFile : metaInfoFiles) {
+				MetaInfo metaInfo = new MetaInfo(metaInfoFile);
+				for (String literal : metaInfo.getMetaConstants().keySet()) {
+					if (literal.equals("tbfl_fk_datatype")) {
+						// DataSourceField 
+						continue; // Skip this one -> do it manual !!
+					}
+					if (metaInfos.putIfAbsent(literal, metaInfo) != null) {
+						throw new RuntimeException("Found override: {}" + literal);
+					}
+				}
+			}
+		}
+		
+		return Collections.unmodifiableMap(metaInfos);
+	}
+}
--- leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/operation/MetaInfoRefactor.java
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/operation/MetaInfoRefactor.java
@@ -1,13 +1,9 @@
 package be.leonix.tools.refactor.operation;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,7 +13,9 @@ import org.slf4j.LoggerFactory;
 
 import be.leonix.tools.refactor.FileRefactor;
 import be.leonix.tools.refactor.RefactorContext;
+import be.leonix.tools.refactor.RefactorMode;
 import be.leonix.tools.refactor.model.SourceFile;
+import be.leonix.tools.refactor.model.SourceLine;
 
 /**
  * A {@link FileRefactor} that refactors code to use the MetaInfo constants. 
@@ -26,97 +24,71 @@ import be.leonix.tools.refactor.model.SourceFile;
  */
 public final class MetaInfoRefactor implements FileRefactor {
 	
-	/**
-	 * @author leonix
-	 */
-	public static final class MetaInfoType {
-		
-		private static final Logger logger = LoggerFactory.getLogger(MetaInfoType.class);
-		
-		private static final Pattern META_INFO_DEF = Pattern.compile(
-				"public static final String ([\\w_]+) = \"([\\w_]+)\";");
-		
-		private String packageID;
-		private String infoClass;
-		private final Map<String, String> metaConstants = new LinkedHashMap<>();
-		
-		public MetaInfoType(File metaInfoFile) {
-			
-			// Get the class-name of the meta-info type.
-			String fileName = metaInfoFile.getName();
-			if (fileName.endsWith("Meta.java")) {
-				infoClass = StringUtils.removeEnd(fileName, "Meta.java");
-				logger.info("Found meta-info class: {}", infoClass);
-			} else {
-				logger.warn("Skipped non-meta-info: {}", fileName);
-				return;
-			}
-			
-			try (FileReader fileReader = new FileReader(metaInfoFile, StandardCharsets.UTF_8)) {
-				try (BufferedReader reader = new BufferedReader(fileReader)) {
-					String line = reader.readLine();
-					
-					// Get the pacage-name of the meta-info type.
-					if (line != null && line.startsWith("package ") && line.endsWith(";")) {
-						packageID = line.substring("package ".length(), line.length() - 1);
-						logger.info("Found meta-info package: {}", packageID);
-					} else {
-						logger.warn("Skipped no package: {}", fileName);
-						return;
-					}
-					
-					line = reader.readLine();
-					while (line != null) {
-						Matcher matcher = META_INFO_DEF.matcher(line);
-						if (matcher.find()) {
-							String constant = matcher.group(1);
-							String literal  = matcher.group(2);
-							metaConstants.put(literal, constant);
-						}
-						line = reader.readLine();
-					}
-				}
-			} catch (RuntimeException | IOException ex) {
-				throw new RuntimeException("Could not parse meta-info-file: " + metaInfoFile, ex);
-			}
-		}
-		
-		public String getInfoClass() {
-			return infoClass;
-		}
-		
-		public String getPackageID() {
-			return packageID;
-		}
-		
-		public Map<String, String> getMetaConstants() {
-			return Collections.unmodifiableMap(metaConstants);
-		}
-	}
-	
 	private static final Logger logger = LoggerFactory.getLogger(MetaInfoRefactor.class);
 	
 	private static final Pattern META_INFO_REF = Pattern.compile("\"([\\w_])\"" );
 	
-	private final Map<String, MetaInfoType> metaInfos = new LinkedHashMap<>();
+	private final Map<String, MetaInfo> metaInfos;
 	
 	public MetaInfoRefactor(File metaInfoDir) {
-		File[] metaInfoFiles = metaInfoDir.listFiles();
-		if (metaInfoFiles != null) {
-			for (File metaInfoFile : metaInfoFiles) {
-				MetaInfoType metaInfo = new MetaInfoType(metaInfoFile);
-				for (String literal : metaInfo.getMetaConstants().keySet()) {
-					if (metaInfos.putIfAbsent(literal, metaInfo) != null) {
-						logger.error("Found literal override: {}", literal);
-					}
-				}
-			}
-		}
+		metaInfos = MetaInfo.getMetaInfo(metaInfoDir);
+		logger.info("Found {} meta-infos.", metaInfos.size());
 	}
 	
 	@Override
 	public void refactorFile(SourceFile sourceFile, RefactorContext context) {
+		long changeCount = 0;
 		
+		Set<MetaInfo> addedMetaInfo = new HashSet<>();
+		for (SourceLine sourceLine : sourceFile.getSourceLines()) {
+			String oldLine = sourceLine.getLineContent();
+			
+			StringBuilder builder = new StringBuilder();
+			if (StringUtils.isNotEmpty(oldLine)) {
+				
+				int offset = 0;
+				Matcher matcher = META_INFO_REF.matcher(oldLine);
+				while (matcher.find(offset)) {
+					// Copy unmatched leading section.
+					if (matcher.start() > offset) {
+						builder.append(oldLine.substring(offset, matcher.start()));
+					}
+					// Execute refactor for pattern.
+					// String reference = matcher.group();
+					for (int group = 1; group <= matcher.groupCount(); group++) {
+						String reference = matcher.group(1);
+						
+						MetaInfo metaInfo = metaInfos.get(reference);
+						if (metaInfo != null) {
+							builder.append(metaInfo.getInfoClass());
+							builder.append(".");
+							builder.append(metaInfo.getMetaConstants().get(reference));
+							
+							changeCount++;
+							addedMetaInfo.add(metaInfo);
+						} else {
+							builder.append(matcher.group());
+						}
+					}
+					offset = matcher.end();
+				}
+				// Copy unmatched trailing section.
+				if (offset < oldLine.length()) {
+					builder.append(oldLine.substring(offset));
+				}
+			}
+			String newLine = builder.toString();
+			if (! StringUtils.equals(oldLine, newLine)) {
+				sourceLine.setLineContent(newLine);
+				changeCount++;
+			}
+		}
+		if (context.getMode() == RefactorMode.UPDATE_FILE && changeCount > 0) {
+			for (MetaInfo metaInfo : addedMetaInfo) {
+				sourceFile.addImportLine(metaInfo.getPackageID() + "." + metaInfo.getInfoClass());
+			}
+			sourceFile.saveContents();
+		}
 	}
 	
 	public static void main(String[] args) {
