05d0af89b0827eb88971969a9c93dda0e2f94d3b
kenaudenaert <ken.audenaert@telenet.be>
2021-09-14 01:03:47 +0200

    Added scaffolding.

--- leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/lang/JavaBlock.java
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/lang/JavaBlock.java
@@ -1,6 +1,8 @@
 package be.leonix.tools.refactor.model.lang;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import be.leonix.tools.refactor.model.repo.SourceLocation;
 
@@ -17,8 +19,49 @@ public final class JavaBlock {
 		IF, ELSE_IF, ELSE
 	}
 	
-	private JavaBlock parent;			// The context block.
-	private SourceLocation start;		// The block-opening: {
-	private SourceLocation end;			// The block-closing: }
-	private List<JavaBlock> children;	// The embedded blocks.
+	private final SourceLocation opening;
+	private final SourceLocation closing;
+	private final List<JavaBlock> children;
+	private BlockType blockType;
+	
+	public JavaBlock(SourceLocation opening, SourceLocation closing) {
+		this.opening = Objects.requireNonNull(opening);
+		this.closing = Objects.requireNonNull(closing);
+		this.children = new ArrayList<>();
+	}
+	
+	/**
+	 * Returns the block-opening (location of '{‘).
+	 */
+	public SourceLocation getOpening() {
+		return opening;
+	}
+	
+	/**
+	 * Returns the block-closing (location of '}‘).
+	 */
+	public SourceLocation getClosing() {
+		return closing;
+	}
+	
+	/**
+	 * Returns the children (sub-blocks) of this block.
+	 */
+	public List<JavaBlock> getChildren() {
+		return children;
+	}
+	
+	/**
+	 * Returns the optional block-type of this block.
+	 */
+	public BlockType getBlockType() {
+		return blockType;
+	}
+	
+	/**
+	 * Changes the optional block-type of this block.
+	 */
+	public void setBlockType(BlockType blockType) {
+		this.blockType = blockType;
+	}
 }
--- leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/lang/JavaFile.java
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/lang/JavaFile.java
@@ -1,9 +1,12 @@
 package be.leonix.tools.refactor.model.lang;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
 import be.leonix.tools.refactor.model.repo.SourceFile;
+import be.leonix.tools.refactor.model.repo.SourceLine;
 
 /**
  * This class encapsulates an editable Java source-file.
@@ -13,12 +16,38 @@ import be.leonix.tools.refactor.model.repo.SourceFile;
 public final class JavaFile {
 	
 	private final SourceFile sourceFile;
-	
-	private String packageID;
-	private List<String> imports;
-	private List<JavaBlock> blocks;
+	private final String packageID;
+	private final List<JavaBlock> blocks;
 	
 	public JavaFile(SourceFile sourceFile) {
 		this.sourceFile = Objects.requireNonNull(sourceFile);
+		
+		Iterator<SourceLine> lines = sourceFile.getSourceLines().iterator();
+		if (! lines.hasNext()) {
+			throw new RuntimeException("Invalid (empty file) Java file: " + sourceFile.getSourceFile());
+		} else {
+			String packageLine = lines.next().getLineContent();
+			
+			// Get the package-name of the meta-info.
+			if (packageLine.startsWith("package ") && packageLine.endsWith(";")) {
+				packageID = packageLine.substring("package ".length(), packageLine.length() - 1);
+			} else {
+				throw new RuntimeException("Invalid (no package) Java file: " + sourceFile.getSourceFile());
+			}
+		}
+		
+		this.blocks = new ArrayList<JavaBlock>();
+	}
+	
+	public SourceFile getSourceFile() {
+		return sourceFile;
+	}
+	
+	public String getPackageID() {
+		return packageID;
+	}
+	
+	public List<JavaBlock> getBlocks() {
+		return blocks;
 	}
 }
