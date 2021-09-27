ce922fb87209b6c7f5450177d39827bb71dba9ac
kenaudenaert <ken.audenaert@telenet.be>
2021-09-27 14:45:42 +0200

    Added SourceChange.

---
+++ leonix-sandbox-all/leonix-tools/src/main/java/be/leonix/tools/refactor/model/repo/SourceChange.java
@@ -0,0 +1,40 @@
+package be.leonix.tools.refactor.model.repo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class defines a source-change that occurrs at several locations.
+ * 
+ * @author Ken Audenaert
+ */
+public final class SourceChange {
+	
+	private final String oldText;
+	private final String newText;
+	private final List<SourceLocation> locations;
+	
+	private SourceChange(String oldText, String newText) {
+		this.oldText = Objects.requireNonNull(oldText);
+		this.newText = Objects.requireNonNull(newText);
+		this.locations = new ArrayList<>();
+	}
+	
+	public String getOldText() {
+		return oldText;
+	}
+	
+	public String getNewText() {
+		return newText;
+	}
+	
+	public List<SourceLocation> getLocations() {
+		return Collections.unmodifiableList(locations);
+	}
+	
+	public void addLocation(SourceLocation location) {
+		locations.add(Objects.requireNonNull(location));
+	}
+}
